2c3ce868da0489d1e0810dea0e35785b8d178e9e
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-03-01 21:28:50 +0200

    Init

---
+++ EigenValues.py
@@ -0,0 +1,174 @@
+from numpy import transpose, diagonal
+from numpy.linalg import norm
+import numpy as np
+import sklearn.datasets
+import Laplacian
+import time
+import math #used only for ceil, check if it's fine
+
+def QR(A):
+
+    n = A.shape[0]
+    R = np.zeros((n,n))
+    Q = np.zeros((n,n))
+    Norm = np.zeros(n)
+    U = A.copy()
+    
+    for i in range(n): 
+        Norm[i] = norm(U[:,i])
+        Q[:,i] = U[:,i]/Norm[i]
+        R[i][i+1:n] = Q[:,i].dot(U[:,i+1:n])
+        U[:,i+1:n] = U[:,i+1:n] - transpose(np.array([Q[:,i]])).dot(np.array([R[i,i+1:n]]))
+
+    np.fill_diagonal(R,Norm)
+    return (Q,R)
+
+def QR_Iterations(A, epsilon = 0.0001): #doesn't converge all the way
+
+    n = A.shape[0]
+    Abar = A.copy()
+    Qbar = np.eye(n,dtype=float)
+    Sum = 0
+
+    for i in range(max(n,10)):
+        t1 = time.time()
+        (Q,R) = QR(Abar)
+        Sum += time.time() - t1
+        Abar = R.dot(Q)
+        
+        diff_arr = abs(diagonal(Qbar))- abs(diagonal(Qbar.dot(Q)))
+        if (abs(diff_arr) <= epsilon).all():
+            return (Abar,Qbar)
+
+        Qbar = Qbar.dot(Q)
+        
+    print("QR time tot is: ", Sum)
+    return (Abar,Qbar)
+    
+def eigengap_test(v, Random, k): #check this function
+    n = len(v)
+    if(not Random):
+        delta = [abs(v[i]-v[i+1]) for i in range(math.ceil(n/2))] #odd values of n?
+        return delta.index(max(delta))
+    return k
+
+def U_matrix(A, Random, k):
+    n = A.shape[0]
+    (Abar,Qbar) = QR_Iterations(A)
+
+    dtype = [('key',float),('value',int)]
+    tup_arr = [(Abar[i][i],i) for i in range(n)]
+    tup_arr = np.array(tup_arr,dtype = dtype)
+    tup_arr = np.sort(tup_arr,order = 'key')
+
+    k = eigengap_test([tup_arr[i][0] for i in range(n)], Random, k)
+    U = [Qbar[:,tup_arr[j][1]] for j in range(k+1)] #should it be k+1 or k? I think k+1.
+    U = np.array(U)
+    U = transpose(U) #why necessary?
+    return U
+
+def T_matrix(A, Random, k):
+    U = U_matrix(A, Random, k)
+    n = U.shape[0]
+    m = U.shape[1]
+    T = [[U[i][j]/norm(U[i]) for j in range(m)] for i in range(n)]
+    return np.array(T)
+
+def Create_vectors_to_cluster(X, Random, k):
+    Lnorm = Laplacian.Lnorm_matrix(X)
+    T = T_matrix(Lnorm, Random, k)
+    return T
+
+###########################################################  CURRENTLY NOT IN USE ####################################################################
+
+def QR_draft(A): #less efficient, not in use
+    n = A.shape[0]
+    R = np.zeros((n,n))
+    Q = np.zeros((n,n))
+    U = A.copy()
+    
+    for i in range(n):
+        norm = np.linalg.norm(U[:,i])
+        R[i][i] = norm
+        Q[:,i] = U[:,i]/norm
+        for j in range(i+1,n): 
+            R[i][j] = Q[:,i].dot(U[:,j])
+            U[:,j] = U[:,j] - (R[i][j]*Q[:,i])
+    return (Q,R)
+
+def multiply_triangular(R,Q): #currently not int use
+    n = R.shape[0]
+    k = R.shape[1]
+    m = Q.shape[1]
+    return np.array([[R[i,:(k-i)].dot(Q[:(k-i),j]) for j in range(m)] for i in range(n)])
+
+######################################################################################################################################################
+
+###########################################################  DEBUG FUNCTIONS #########################################################################
+
+def debug_general():
+    A = np.array([(5.0,3.0,1.0,4.0),(3.0,6.0,0.0,2.5),(1.0,0.0,3.0,1.7),(4.0,2.5,1.7,10.0)])
+    #A = np.full((3,3),2)
+    (Q,R) = QR(A)
+    print("Q is:")
+    print(Q)
+    print("R is:")
+    print(R)
+    print("The QR multiplication is:")
+    print(Q.dot(R))
+    print("")
+    (Abar,Qbar) = QR_Iterations(A)
+    print(Abar)
+    print("")
+    #print("")
+    #print(Qbar)
+    #print("")
+    #print(U_matrix(A))
+    #print(T_matrix(U_matrix(A)))
+    print(np.linalg.eigvalsh(A))
+    #(Abar,Qbar) = QR_Iterations(A)
+    #print(Abar)
+    #A = np.array([(5.0,3.0),(3.0,6.0)])
+    #print("")
+    #[Q,R] = QR(A)
+    #print(np.matmul(np.transpose(Q),Q))
+    #print(R)
+    #print(np.matmul(Q,R))
+    return 0
+
+#print(Create_vectors_to_cluster(np.random.rand(4,400)))
+
+def debug_QR():
+    lst = []
+    for i in range(50):
+        A = np.random.rand(i,i)
+        (Q,R) = QR(A)
+        lst.append((abs(A - np.matmul(Q,R)) < 0.00000001).all())
+    print(lst)
+
+def debug_QRIterations(): 
+    lst = []
+    for i in range(1):
+        #A = np.random.rand(50,3)*0.1
+        t1 = time.time()
+        A = sklearn.datasets.make_blobs(n_samples = 200,n_features = 12,centers = 12)[0]
+        Lnorm = Laplacian.Lnorm_matrix(A)
+        T = T_matrix(Lnorm, False,0)
+        t2 = time.time()
+        print("The time is approximatly ", t2 - t1, " seconds")
+        #(Abar,Qbar) = QR_Iterations(Lnorm)
+        #eig_sorted_1 = [Abar[j][j] for j in range(Lnorm.shape[0])]
+        #eig_sorted_1.sort()
+        #eig_sorted_2 = np.linalg.eigvalsh(Lnorm)
+        #eig_sorted_2.sort()
+        #lst.append(max([abs(eig_sorted_1[j] - eig_sorted_2[j]) for j in range(len(eig_sorted_1))]))
+        print(np.array(T).shape[1])
+    #print("")
+    #print(lst)
+        
+######################################################################################################################################################
+
+debug_QRIterations()
+#debug_QR()
+
+
---
+++ kmeans.c
@@ -0,0 +1,330 @@
+/*
+ * kmeans.c
+ *
+ *  Created on: 25.2.21
+ *  Author: Dori Rimon & Yonatan Lin
+ */
+#define PY_SSIZE_T_CLEAN
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <Python.h>
+
+static double norm();
+static void initialize();
+static void clusters_assignments();
+static void clustering();
+static void update_centroids();
+static void print_centroids();
+static void deallocate_imatrix();
+static void deallocate_dmatrix();
+static PyObject* kmeans();
+
+static int k, N, d, MAX_ITER;
+
+/* --------------- Memory Stuff --------------- */
+
+static int** allocate_imatrix(int rows, int cols) {
+    int i;
+    int **matrix = calloc(rows, sizeof(int*));
+    assert(matrix);
+    for (i = 0; i < rows; i++) {
+        matrix[i] = calloc(cols, sizeof(int));
+        assert(matrix[i]);
+    }
+    return matrix;
+}
+
+static void deallocate_imatrix(int** matrix, int rows)
+{
+    int i;
+    for(i = 0 ; i < rows ; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+static double** allocate_dmatrix(int rows, int cols) {
+    int i;
+    double **matrix = calloc(rows, sizeof(double*));
+    assert(matrix);
+    for (i = 0; i < rows; i++) {
+        matrix[i] = calloc(cols, sizeof(double));
+        assert(matrix[i]);
+    }
+    return matrix;
+}
+
+static void deallocate_dmatrix(double** matrix, int rows)
+{
+    int i;
+    for(i = 0 ; i < rows ; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+static void vector_dassign(double *u, double *v, int size) {
+    int i;
+    for (i=0; i<size; i++) {
+        u[i] = v[i];
+    }
+}
+
+static int m(double** obs, double** cents, int** cltrs) {
+    double **observations = obs;
+    double **centroids = cents;
+    int **clusters = cltrs;
+
+    clustering(observations, clusters, centroids);
+    /*print_centroids(centroids);*/
+    /*FILE* clusters_file = clusters_file(clusters); /*creates the textual representations of the data without k in the first line*/
+    /* deallocating matrices **/
+    deallocate_dmatrix(observations,N);
+    deallocate_dmatrix(centroids,k);
+    deallocate_imatrix(clusters,k);
+    fclose(clusters_file); /* deallocation needed?*/
+    return 0;
+}
+
+static FILE* clusters_file(double** clusters) /*currently not in use*/
+{
+	int i;
+	int j;
+	FILE* clusters_file = fopen("Clusters_rep.txt","w");
+	/*each row in clusters is a cluster, we put 1 for the columns representing the observations which are in this cluster*/
+	for(i = 0 ; i < N  ; i++)
+	{
+		if(i > 0)
+		{
+			putc(clusters_file,"\n");
+		}
+
+		for(j = 0 ; j < N ; j++)
+		{
+			if(clusters[i][j] == 1) /*obs j is in cluster i*/
+			{
+				putc(clusters_file,j);
+				putc(clusters_file,",");
+			}
+		}
+	}
+	return clusters_file;
+}
+
+static void initialize(int* init_centroids, double** centroids, double** observations, int** clusters)
+{
+    int i;
+    for (i = 0; i < k; i++) {
+        int index = init_centroids[i];
+        /* printf("index = %i\n", index); */
+        clusters[i][index] = 1;
+        vector_dassign(centroids[i], observations[index], d);
+        centroids[i][d] = i;
+    }
+}
+
+static void update_centroids(double **centroids, double** observations) {
+    int i;
+    double **sum = allocate_dmatrix(k, d);
+    int r;
+    int *counter = (int *)calloc(k, sizeof(int));
+    assert(sum);
+
+    for (i = 0; i < N; i++)
+    {
+        int cluster = observations[i][d];
+        int j;
+        counter[cluster]++;
+        for (j = 0; j < d; j++)
+        {
+            sum[cluster][j] += observations[i][j];
+        }
+    }
+    for (i = 0; i < k; i++)
+    {
+        for (r = 0; r < d; r++) {
+            sum[i][r] /= counter[i];
+        }
+        vector_dassign(centroids[i], sum[i], d);
+    }
+    deallocate_dmatrix(sum, k);
+    free(counter);
+}
+
+static void clusters_assignments(int i, int prev, int val, double **observations, int **clusters) {
+    observations[i][d] = val;
+    if(prev >= 0)
+    {
+        clusters[prev][i] = 0;
+    }
+    clusters[val][i] = 1;
+}
+
+static void clustering(double **observations, int **clusters, double **centroids) {
+    int iter = 0;
+    int flag = 0;
+
+    while (iter < MAX_ITER && flag != 1) {
+        int i;
+        flag = 1;
+
+        for (i = 0; i < N; i++) {
+            int j = 0;
+            int r;
+            double n = norm(observations[i], centroids[0], d);
+
+            for (r = 1; r < k; r++) {
+                double val = norm(observations[i], centroids[r], d);
+                if (val < n) {
+                    n = val;
+                    j = r;
+                }
+            }
+
+            if (j != observations[i][d]) {
+                flag = 0;
+                clusters_assignments(i, observations[i][d], j, observations, clusters);
+            }
+        }
+
+        update_centroids(centroids, observations);
+        iter++;
+    }
+
+}
+
+static double norm(double *v1, double *v2, int length) {
+    double sum = 0;
+    int i;
+    for (i = 0; i < length; ++i) {
+        sum = sum + (v2[i] - v1[i]) * (v2[i] - v1[i]);
+    }
+    return sum;
+}
+
+static void print_centroids(double **centroids) {
+    int i;
+    for (i = 0; i < k; i++) {
+        int j;
+        for (j = 0; j < d; j++) {
+            printf("%f", centroids[i][j]);
+            if (j < d - 1) {
+                printf(",");
+            }
+        }
+        printf("\n");
+    }
+}
+
+/* --------------- Python Stuff --------------- */
+
+/*
+    input: double** observations, int* centroids, int k, int N, int d, int MAX_ITER
+*/
+static PyObject* kmeans(PyObject *self, PyObject *args) {
+
+    Py_ssize_t i, j;
+    PyObject *_list;
+    PyObject *item;
+    PyObject *py_observations;
+    PyObject *py_centroids;
+    double** observations;
+    int* init_centroids;
+    double** centroids;
+    int** clusters;
+    double*** res;
+
+    if(!PyArg_ParseTuple(args, "O:kmeans", &_list)) {
+        return NULL;
+    }
+
+    if (!PyList_Check(_list))
+        return NULL;
+
+    py_observations = PyList_GetItem(_list, 0);
+    py_centroids = PyList_GetItem(_list, 1);
+    k = PyLong_AsLong(PyList_GetItem(_list, 2));
+    N = PyLong_AsLong(PyList_GetItem(_list, 3));
+    d = PyLong_AsLong(PyList_GetItem(_list, 4));
+    MAX_ITER = PyLong_AsLong(PyList_GetItem(_list, 5));
+
+    observations = allocate_dmatrix(N, d+1);
+    init_centroids = malloc(sizeof(int) * k);
+    centroids = allocate_dmatrix(k, d+1);
+    clusters = allocate_imatrix(k, N);
+
+    for (i = 0; i < N; i++) {
+        item = PyList_GetItem(py_observations, i);
+        for (j = 0; j < d; j++) {
+            observations[i][j] = PyFloat_AsDouble(PyList_GetItem(item, j));
+        }
+        observations[i][d] = -1;
+    }
+
+    for (i = 0; i < k; i++) {
+        item = PyList_GetItem(py_centroids, i);
+        init_centroids[i] = PyLong_AsLong(item);
+    }
+
+    initialize(init_centroids, centroids, observations, clusters);
+    m(observations, centroids, clusters); /*create the observation, centroids and clusters inplace*/
+    /*now need to return a python tuple of observations,clusters*/
+
+
+    free(init_centroids);
+    Py_RETURN_NONE;
+}
+
+
+static PyObject* lst_to_pylst(double* lst, int len) /*convert C list to python list*/
+{
+	PyObject *_list;
+	int i;
+	for(i = 0 ; i < len ; i++)
+	{
+		PyList_Insert(_list,i,lst[i]);
+	}
+	return PyObject*;
+}
+
+static PyObject* matrix_to_pymatrix(double** mat, int rows, int columns)
+{
+	PyObject* _matrix;
+	int i;
+	int j;
+	for(i = 0 ; i < rows ; i++)
+	{
+		PyList_Insert()
+	}
+}
+
+
+/*
+ * A macro to help us with defining the methods
+ * Compare with: {"f1", (PyCFunction)f1, METH_NOARGS, PyDoc_STR("No input parameters")}
+*/
+#define FUNC(_flag, _name, _docstring) { #_name, (PyCFunction)_name, _flag, PyDoc_STR(_docstring) }
+
+static PyMethodDef _methods[] = {
+    {"kmeans", (PyCFunction)kmeans, METH_VARARGS, PyDoc_STR("6 input parameters")},
+    {NULL, NULL, 0, NULL}   /* sentinel */
+};
+
+static struct PyModuleDef _moduledef = {
+    PyModuleDef_HEAD_INIT,
+    "mykmeanssp",
+    NULL,
+    -1,
+    _methods
+};
+
+PyMODINIT_FUNC
+PyInit_mykmeanssp(void)
+{
+    return PyModule_Create(&_moduledef);
+}
+
