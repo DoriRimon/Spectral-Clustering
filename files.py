0e4e463bde1ac3c55c8c45f03550b5329bc7015a
DoriRimon <67804152+DoriRimon@users.noreply.github.com>
2021-03-25 21:54:58 +0200

    Jaccard work

--- files.py
+++ files.py
@@ -69,7 +69,7 @@ def jaccard(data, result, k):
 
 	counter = 0
 	for i in range(n):
-		for j in range(n):
+		for j in range(i+1, n):
 			if data_hash[i] == data_hash[j] and result_hash[i] == result_hash[j]:
 				counter += 1
 
